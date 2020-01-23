ec527e36292a1f0292c5fb991014591d9e054cd0
Luke <lmschless@gmail.com>
2020-01-22 20:53:04 -0800

    added basic jquery scroll event to fade in my name on index.html. Added <br> height for separation. Worked on nav, added icon, added buttons. Added header scroll effect.

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-document.addEventListener('DOMContentLoaded', function() {
-	var elems = document.querySelectorAll('.sidenav');
-	var instances = M.Sidenav.init(elems, options);
+$(document).ready(function() {
+	$(document).scroll(function() {
+		$('.hidden').removeClass('hidden');
+	});
 });
